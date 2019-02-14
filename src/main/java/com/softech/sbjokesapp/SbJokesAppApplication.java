7157c1d32d33becba13fc42f38750b59cb83c006
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-02-14 16:22:10 +0700

    first commit

---
+++ src/main/java/com/softech/sbjokesapp/SbJokesAppApplication.java
@@ -0,0 +1,14 @@
+package com.softech.sbjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbJokesAppApplication.class, args);
+    }
+
+}
+
